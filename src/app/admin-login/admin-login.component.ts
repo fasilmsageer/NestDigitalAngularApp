71cd8f3d7841ebb2968aab581aa41aa5443a109f
fasilmsageer <fasilmsageer@gmail.com>
2022-11-30 12:51:03 +0530

    adminlogin

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent {
+
+  username=""
+  password=""
+
+  
+}
