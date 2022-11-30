d43504d3927688cf946dc8800b692520f8152737
fasilmsageer <fasilmsageer@gmail.com>
2022-11-30 14:42:46 +0530

    employeereg

---
+++ src/app/employeereg/employeereg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employeereg',
+  templateUrl: './employeereg.component.html',
+  styleUrls: ['./employeereg.component.css']
+})
+export class EmployeeregComponent {
+
+}
