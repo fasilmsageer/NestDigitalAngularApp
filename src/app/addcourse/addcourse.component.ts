dc07394626c3a268f9fe9eaee7399237b891dadc
fasilmsageer <fasilmsageer@gmail.com>
2022-11-30 15:17:53 +0530

    addcourse

---
+++ src/app/addcourse/addcourse.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addcourse',
+  templateUrl: './addcourse.component.html',
+  styleUrls: ['./addcourse.component.css']
+})
+export class AddcourseComponent {
+
+  courseTitle=""
+  courseDescription=""
+  courseDuration=""
+  courseDate=""
+  courseVenue=""
+
+  
+  readValue=()=>
+  {
+    let data:any={"courseTitle":this.courseTitle,"courseDescription":this.courseDescription,"courseDuration":this.courseDuration,"courseDate":this.courseDate,"courseVenue":this.courseVenue}
+    console.log(data)
+  }
+
+
+}
